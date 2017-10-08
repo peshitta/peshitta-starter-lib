cf26281b04db7a2763d6542b0f0a6496aa02d58a
Greg Borota <borota@gmail.com>
2017-10-08 17:03:34 -0500

    Remove invalid char from mainMin

--- rollup.config.js
+++ rollup.config.js
@@ -82,7 +82,7 @@ if (isProduction) {
   // browser/nodejs-friendly minified UMD build
   targets.push({
     input,
-    output: [{ file: pkg.main݂Min, format }],
+    output: [{ file: pkg.mainMin, format }],
     external,
     plugins,
     name,
